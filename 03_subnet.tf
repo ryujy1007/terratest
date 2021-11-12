e811a0e96858f9e2d2c750bb8bbe9c570d0a3f7e
ryujy1007 <rrrrrrrjy1007@gmail.com>
2021-11-12 16:16:35 +0900

    step2

--- 03_subnet.tf
+++ 03_subnet.tf
@@ -37,3 +37,23 @@ resource "aws_subnet" "ryujy_pric" {
     "Name" = "ryujy-pric"
   }
 }
+
+# 가용영역 a의 private DB subnet
+resource "aws_subnet" "ryujy_pridba" {
+  vpc_id = aws_vpc.ryu.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "ap-northeast-2a"
+  tags = {
+    "Name" = "ryujy-pridba"
+  }
+}
+
+# 가용영역 c의 private DB subnet
+resource "aws_subnet" "ryujy_pridbc" {
+  vpc_id = aws_vpc.ryu.id
+  cidr_block = "10.0.5.0/24"
+  availability_zone = "ap-northeast-2c"
+  tags = {
+    "Name" = "ryujy-pridbc"
+  }
+}
